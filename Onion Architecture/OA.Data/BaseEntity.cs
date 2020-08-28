8c4ee4783a20421a33414ff85681ced528839180
isakordis <isa.kordis02@gmail.com>
2020-08-28 23:01:13 +0300

    First Commit
    Add repository and domain layers

---
+++ Onion Architecture/OA.Data/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OA.Data
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string IpAdress { get; set; }
+
+
+
+    }
+}
---
+++ Onion Architecture/OA.Repository/Repository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OA.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OA.Repository
+{
+    public class Repository<T>:IRepository<T> where T:BaseEntity
+    {
+        private readonly ApplicationContext context;
+        private DbSet<T> entities;
+
+        public Repository(ApplicationContext applicationContext)
+        {
+            this.context = applicationContext;
+            entities=context.Set<T>();
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public T Get(long id)
+        {
+            return entities.SingleOrDefault(sa=>sa.Id==id);
+
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public void Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            context.Remove(entity);
+        }
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            context.SaveChanges();
+        }
+    }
+}
